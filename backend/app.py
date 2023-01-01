eef9a0e4cbc32f6a8f94250ac8597b3b9d03e5e4
adrgs <albastroiudragos@gmail.com>
2023-01-01 16:00:51 +0100

    Format documents using yapf

--- backend/app.py
+++ backend/app.py
@@ -12,11 +12,13 @@ import os
 import sys
 from pprint import pprint
 
+
 class CustomServerHeaderFlask(Flask):
     def process_response(self, response):
         response.headers['Server'] = 'requestrepo.com'
         super(CustomServerHeaderFlask, self).process_response(response)
-        return(response)
+        return (response)
+
 
 app = CustomServerHeaderFlask(__name__, static_url_path='/public/static')
 app.url_map.add(Rule('/', endpoint='index'))
@@ -25,56 +27,74 @@ app.url_map.add(Rule('/<path:path>', endpoint='catch_all'))
 JWT_SECRET = 'changethis'
 DOMAIN = os.getenv('DOMAIN', 'requestrepo.com')
 
+
 def verify_jwt(token):
     try:
         if token:
-            return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])['subdomain']
+            return jwt.decode(token, JWT_SECRET,
+                              algorithms=['HS256'])['subdomain']
     except:
         return None
     return None
 
-def write_basic_file(subdomain):
-    file_data = {'headers':[{'header':'Access-Control-Allow-Origin','value':'*'},{'header':'Content-Type','value':'text/html'}], 'status_code':200, 'raw':''}
 
-    with open('pages/'+subdomain, 'w') as outfile:
+def write_basic_file(subdomain):
+    file_data = {
+        'headers': [{
+            'header': 'Access-Control-Allow-Origin',
+            'value': '*'
+        }, {
+            'header': 'Content-Type',
+            'value': 'text/html'
+        }],
+        'status_code':
+        200,
+        'raw':
+        ''
+    }
+
+    with open('pages/' + subdomain, 'w') as outfile:
         json.dump(file_data, outfile)
+
 
 def log_request(request, subdomain):
     dic = {}
     headers = dict(request.headers)
 
-    dic['raw']       = request.stream.read()
-    dic['uid']       = subdomain
-    dic['ip']        = request.remote_addr
-    dic['headers']   = headers
-    dic['method']    = request.method
-    dic['protocol']  = request.environ.get('SERVER_PROTOCOL')
+    dic['raw'] = request.stream.read()
+    dic['uid'] = subdomain
+    dic['ip'] = request.remote_addr
+    dic['headers'] = headers
+    dic['method'] = request.method
+    dic['protocol'] = request.environ.get('SERVER_PROTOCOL')
     if request.full_path[-1] == '?' and request.url[-1] != '?':
-        dic['path']  = request.full_path[:-1]
+        dic['path'] = request.full_path[:-1]
     else:
-        dic['path']  = request.full_path
+        dic['path'] = request.full_path
     if dic['path'].find('?') > -1:
         dic['query'] = dic['path'][dic['path'].find('?'):]
     else:
         dic['query'] = ''
-    dic['url']       = request.url
-    dic['date']      = datetime.datetime.utcnow()
+    dic['url'] = request.url
+    dic['date'] = datetime.datetime.utcnow()
 
     http_insert_into_db(dic)
 
+
 def get_subdomain_from_hostname(host):
-    subdomain = host[:-len(DOMAIN)-1][-8:]
+    subdomain = host[:-len(DOMAIN) - 1][-8:]
     if not subdomain or not subdomain.isalnum():
         return None
-    
+
     return subdomain.lower()
+
 
 def subdomain_response(request, subdomain):
     log_request(request, subdomain)
-    data = {'raw':'', 'headers':[], 'status_code':200}
-    if not os.path.exists('pages/'+subdomain):
+    data = {'raw': '', 'headers': [], 'status_code': 200}
+    if not os.path.exists('pages/' + subdomain):
         write_basic_file(subdomain)
-    with open('pages/'+subdomain,'r') as json_file:
+    with open('pages/' + subdomain, 'r') as json_file:
         try:
             data = json.load(json_file)
         except:
@@ -101,6 +121,7 @@ def index():
         subdomain = verify_jwt(request.cookies.get('token'))
         return send_from_directory('public', 'index.html')
 
+
 @app.endpoint('catch_all')
 def catch_all(path):
     if request.host == DOMAIN:
@@ -112,7 +133,7 @@ def catch_all(path):
     else:
         rindex = request.host.rfind(':')
         port = ':80'
-        nr = request.host[rindex+1:]
+        nr = request.host[rindex + 1:]
         if nr.isdigit():
             if int(nr) <= 20000:
                 port = ':' + nr
@@ -130,7 +151,8 @@ def get_dns_requests():
         if subdomain:
             return jsonify(dns_get_subdomain(subdomain, time))
         else:
-            return jsonify({'err':'Unauthorized'}), 401
+            return jsonify({'err': 'Unauthorized'}), 401
+
 
 @app.route('/api/get_http_requests')
 def get_http_requests():
@@ -143,7 +165,8 @@ def get_http_requests():
         if subdomain:
             return jsonify(http_get_subdomain(subdomain, time))
         else:
-            return jsonify({'err':'Unauthorized'}), 401
+            return jsonify({'err': 'Unauthorized'}), 401
+
 
 @app.route('/api/get_requests')
 def get_requests():
@@ -163,9 +186,10 @@ def get_requests():
                 'date': server_time
             })
         else:
-            return jsonify({'err':'Unauthorized'}), 401
+            return jsonify({'err': 'Unauthorized'}), 401
 
-@app.route('/api/get_token', methods=['POST','OPTIONS'])
+
+@app.route('/api/get_token', methods=['POST', 'OPTIONS'])
 def get_token():
     subdomain = get_subdomain_from_hostname(request.host)
     if subdomain:
@@ -186,10 +210,11 @@ def get_token():
             'exp': datetime.datetime.utcnow() + datetime.timedelta(days=31),
             'subdomain': subdomain
         }
-        token = jwt.encode(payload,JWT_SECRET,algorithm='HS256')
+        token = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
         resp = make_response(token)
         resp.set_cookie('token', token)
         return resp
+
 
 @app.route('/api/get_server_time')
 def get_server_time():
@@ -197,7 +222,8 @@ def get_server_time():
     if subdomain:
         return subdomain_response(request, subdomain)
     else:
-        return jsonify({'date':datetime.datetime.utcnow().strftime('%s')})
+        return jsonify({'date': datetime.datetime.utcnow().strftime('%s')})
+
 
 @app.route('/api/delete_request', methods=['POST'])
 def delete_request():
@@ -212,8 +238,9 @@ def delete_request():
                 _id = content.get('id')
                 rtype = content.get('type')
                 delete_request_from_db(_id, subdomain, rtype)
-                return jsonify({"rtype":rtype, "_id":_id})
+                return jsonify({"rtype": rtype, "_id": _id})
         return jsonify("error")
+
 
 @app.route('/api/get_file', methods=['GET'])
 def get_file():
@@ -222,10 +249,11 @@ def get_file():
         return subdomain_response(request, subdomain)
     subdomain = verify_jwt(request.cookies.get('token'))
     if subdomain:
-        with open('pages/'+subdomain, 'r') as outfile:
+        with open('pages/' + subdomain, 'r') as outfile:
             return outfile.read()
     else:
         return '{"raw":"", "headers":[], status_code:200}'
+
 
 @app.route('/api/update_file', methods=['POST'])
 def update_file():
@@ -239,7 +267,7 @@ def update_file():
         if 'status_code' in content:
             try:
                 try:
-                    if len(content['status_code'])>9:
+                    if len(content['status_code']) > 9:
                         return '{"error":"invalid status_code"}'
                     status_code = int(content['status_code'])
                 except:
@@ -256,18 +284,27 @@ def update_file():
                     return '{"error":"invalid response"}'
             else:
                 return '{"error":"response should be smaller than 2MB"}'
-        headers=[]
+        headers = []
         if 'headers' in content:
             if len(headers) <= 30:
                 for header in content['headers']:
                     if 'header' in header and 'value' in header:
-                        headers.append({'header': header['header'], 'value':header['value']})
+                        headers.append({
+                            'header': header['header'],
+                            'value': header['value']
+                        })
             else:
                 return '{"error":"maximum of 30 headers"}'
-            with open('pages/'+subdomain, 'w') as outfile:
-                json.dump({'headers':headers, 'raw':raw, 'status_code': status_code}, outfile)
+            with open('pages/' + subdomain, 'w') as outfile:
+                json.dump(
+                    {
+                        'headers': headers,
+                        'raw': raw,
+                        'status_code': status_code
+                    }, outfile)
         return '{"msg":"Updated response"}'
     return '{"error":"unauthenticated"}'
+
 
 @app.route('/api/get_dns_records', methods=['GET'])
 def get_dns_records():
@@ -279,7 +316,9 @@ def get_dns_records():
         return jsonify(dns_get_records(subdomain))
     return '{"error":"unauthenticated"}'
 
+
 DNS_RECORDS = ['A', 'AAAA', 'CNAME', 'TXT']
+
 
 @app.route('/api/update_dns_records', methods=['POST'])
 def update_dns_records():
@@ -301,7 +340,7 @@ def update_dns_records():
                     continue
                 if domain == "" or value == "":
                     continue
-                domain=domain.lower()
+                domain = domain.lower()
                 try:
                     if len(domain) > 63:
                         return '{"error":"Domain too big"}'
@@ -318,19 +357,22 @@ def update_dns_records():
                     if not re.search("^[ -~]+$", value):
                         return '{"error":"Invailid regex1"}'
                         continue
-                    if not re.match("^[A-Za-z0-9](?:[A-Za-z0-9\\-_\\.]{0,61}[A-Za-z0-9])?$", domain):
+                    if not re.match(
+                            "^[A-Za-z0-9](?:[A-Za-z0-9\\-_\\.]{0,61}[A-Za-z0-9])?$",
+                            domain):
                         return '{"error":"invalid regex2"}'
                         continue
                     domain = domain + '.' + subdomain + '.requestrepo.com.'
                     dtype = DNS_RECORDS[dtype]
                     dns_insert_record(subdomain, domain, dtype, value)
                 except Exception as e:
-                    return '{"error":"'+str(e)+'"}'
+                    return '{"error":"' + str(e) + '"}'
                     continue
             return '{"msg":"Updated records"}'
         return '{"error":"Invalid records"}'
 
     return '{"error":"unauthenticated"}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=21337, debug=True)
--- backend/mongolog.py
+++ backend/mongolog.py
@@ -28,101 +28,136 @@ else:
 username = urllib.parse.quote_plus(MONGODB_USERNAME)
 password = urllib.parse.quote_plus(MONGODB_PASSWORD)
 
-client = MongoClient('mongodb://%s:%s@%s' % (username, password, MONGODB_HOSTNAME), 27017)
+client = MongoClient(
+    'mongodb://%s:%s@%s' % (username, password, MONGODB_HOSTNAME), 27017)
 db = client[MONGODB_DATABASE]
 
 # DNS Database
 collection = db['dns_requests']
 ddns = db['ddns']
+
+
 def dns_insert_into_db(value):
     value['_deleted'] = False
     collection.insert_one(value)
+
+
 def dns_get_from_db():
-    return collection.find({'_deleted':False}, {'_deleted':False})
+    return collection.find({'_deleted': False}, {'_deleted': False})
 
 
 def dns_get_records(subdomain):
     l = []
-    for x in ddns.find({'subdomain':subdomain}):
+    for x in ddns.find({'subdomain': subdomain}):
         x['_id'] = str(x['_id'])
         l.append(x)
     return l
 
+
 def dns_delete_records(subdomain):
-    ddns.delete_many({'subdomain':subdomain})
+    ddns.delete_many({'subdomain': subdomain})
+
 
 def dns_insert_record(subdomain, domain, dtype, val):
-    ddns.insert_one({'subdomain':subdomain,'domain':domain, 'type':dtype, 'value':val})
+    ddns.insert_one({
+        'subdomain': subdomain,
+        'domain': domain,
+        'type': dtype,
+        'value': val
+    })
+
 
 def dns_get_subdomain(subdomain, time):
     l = []
 
-    find = {'uid':subdomain, '_deleted': False}
+    find = {'uid': subdomain, '_deleted': False}
     try:
         if time != None:
-            find['date'] = {
-                '$gte': datetime.datetime.fromtimestamp(int(time))
-            }
+            find['date'] = {'$gte': datetime.datetime.fromtimestamp(int(time))}
     except:
         pass
 
     for x in collection.find(find, {'_deleted': False}):
         x['_id'] = str(x['_id'])
-        x['raw'] = str(base64.b64encode(x['raw']),'utf-8')
+        x['raw'] = str(base64.b64encode(x['raw']), 'utf-8')
         x['date'] = x['date'].strftime('%s')
         l.append(x)
     return l
+
+
 def dns_delete_records(subdomain):
-    ddns.delete_many({'subdomain':subdomain})
+    ddns.delete_many({'subdomain': subdomain})
+
+
 def dns_delete_request(_id, subdomain):
-    collection.update_one({'uid':subdomain, '_id':ObjectId(_id)}, {'$set':{'_deleted':True}})
+    collection.update_one({
+        'uid': subdomain,
+        '_id': ObjectId(_id)
+    }, {'$set': {
+        '_deleted': True
+    }})
 
 
 # HTTP database
 
 http = db['http']
+
+
 def http_insert_into_db(dic):
     dic['_deleted'] = False
     http.insert_one(dic)
+
+
 def http_get_from_db():
     l = []
-    for x in http.find({'_deleted':False}):
+    for x in http.find({'_deleted': False}):
         x['_id'] = str(x['_id'])
-        x['raw'] = str(base64.b64encode(x['raw']),'utf-8')
+        x['raw'] = str(base64.b64encode(x['raw']), 'utf-8')
         x['date'] = x['date'].strftime('%s')
         l.append(x)
     return l
+
+
 def http_get_subdomain(subdomain, time):
     l = []
 
-    find = {'uid':subdomain,'_deleted':False}
+    find = {'uid': subdomain, '_deleted': False}
     try:
         if time != None:
-            find['date'] = {
-                '$gte': datetime.datetime.fromtimestamp(int(time))
-            }
+            find['date'] = {'$gte': datetime.datetime.fromtimestamp(int(time))}
     except:
         pass
 
     #for x in http.find(find, {'_id': False}):
     for x in http.find(find, {'_deleted': False}):
         x['_id'] = str(x['_id'])
-        x['raw'] = str(base64.b64encode(x['raw']),'utf-8')
+        x['raw'] = str(base64.b64encode(x['raw']), 'utf-8')
         x['date'] = x['date'].strftime('%s')
         l.append(x)
     return l
+
+
 def http_delete_request(_id, subdomain):
-    http.update_one({'_id':ObjectId(_id),'uid':subdomain}, {'$set':{'_deleted':True}})
+    http.update_one({
+        '_id': ObjectId(_id),
+        'uid': subdomain
+    }, {'$set': {
+        '_deleted': True
+    }})
 
 
 # Users Database
 
 users = db['users']
 
+
 def users_insert_into_db(ip, subdomain):
-    collection.insert_one({'ip':ip, 'subdomain':subdomain})
+    collection.insert_one({'ip': ip, 'subdomain': subdomain})
+
+
 def users_get_subdomain(subdomain):
-    return users.find_one({'subdomain':subdomain})
+    return users.find_one({'subdomain': subdomain})
+
 
 def delete_request_from_db(_id, subdomain, dtype):
     if dtype == 'HTTP':
