399245548da64af046f76b0674e68c13dc66870c
adrgs <albastroiudragos@gmail.com>
2023-01-01 16:45:31 +0100

    Update headers.json location

--- frontend/src/service/HeaderService.js
+++ frontend/src/service/HeaderService.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 export class HeaderService {
 
     getHeaders(_this) {
-        return axios.get('assets/demo/data/headers.json')
+        return axios.get('/assets/data/headers.json')
             .then(res => res.data.data)
             .then(data => {
                 _this.setState({ headersData: data });
                 return data;
             });
     }
+
 }
