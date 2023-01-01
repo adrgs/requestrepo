99890c529c89299f886868d941df0ff2ccc0cf29
adrgs <albastroiudragos@gmail.com>
2023-01-01 17:32:29 +0100

    Fix localdate conversion

--- frontend/src/AppSidebar.js
+++ frontend/src/AppSidebar.js
@@ -54,12 +54,11 @@ export class AppSidebar extends Component {
     }
 
     convertUTCDateToLocalDate(date) {
-        //var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
-        //var offset = date.getTimezoneOffset() / 60;
-        //var hours = date.getHours();
-        //newDate.setHours(hours - offset);
-        //return newDate;
-        return date;
+        var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+        var offset = date.getTimezoneOffset() / 60;
+        var hours = date.getHours();
+        newDate.setHours(hours - offset);
+        return newDate;
     }
 
     getRequests() {
