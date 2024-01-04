093851cd3009be6b367474268d531d1c721fe813
adrgs <albastroiudragos@gmail.com>
2024-01-04 18:59:45 +0100

    Remove unused code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,7 @@ import "react-app-polyfill/ie11";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import * as serviceWorker from './serviceWorker';
 import { HashRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,8 +10,3 @@ root.render(
     <App></App>
   </HashRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
