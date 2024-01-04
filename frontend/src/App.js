b6ffcad89ab445135920b36dbf2a7c696574d5f1
adrgs <albastroiudragos@gmail.com>
2024-01-04 18:52:12 +0100

    Remove unused createMenu() function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -229,7 +229,6 @@ class App extends Component {
     this.updateSearchValue = this.updateSearchValue.bind(this);
     this.doRerender = this.doRerender.bind(this);
     this.markAllAsVisited = this.markAllAsVisited.bind(this);
-    this.createMenu();
   }
 
   markAllAsVisited() {
@@ -449,121 +448,6 @@ class App extends Component {
         mobileMenuActive: false,
       });
     }
-  }
-
-  createMenu() {
-    this.menu = [
-      {
-        label: "Dashboard",
-        icon: "pi pi-fw pi-home",
-        command: () => {
-          window.location = "#/";
-        },
-      },
-      {
-        label: "Menu Modes",
-        icon: "pi pi-fw pi-cog",
-        items: [
-          { label: "Static Menu", icon: "pi pi-fw pi-bars", command: () => this.setState({ layoutMode: "static" }) },
-          { label: "Overlay Menu", icon: "pi pi-fw pi-bars", command: () => this.setState({ layoutMode: "overlay" }) },
-        ],
-      },
-      {
-        label: "Menu Colors",
-        icon: "pi pi-fw pi-align-left",
-        items: [
-          { label: "Dark", icon: "pi pi-fw pi-bars", command: () => this.setState({ layoutColorMode: "dark" }) },
-          { label: "Light", icon: "pi pi-fw pi-bars", command: () => this.setState({ layoutColorMode: "light" }) },
-        ],
-      },
-      {
-        label: "Components",
-        icon: "pi pi-fw pi-globe",
-        badge: "9",
-        items: [
-          { label: "Sample Page", icon: "pi pi-fw pi-th-large", to: "/sample" },
-          { label: "Forms", icon: "pi pi-fw pi-file", to: "/forms" },
-          { label: "Data", icon: "pi pi-fw pi-table", to: "/data" },
-          { label: "Panels", icon: "pi pi-fw pi-list", to: "/panels" },
-          { label: "Overlays", icon: "pi pi-fw pi-clone", to: "/overlays" },
-          { label: "Menus", icon: "pi pi-fw pi-plus", to: "/menus" },
-          { label: "Messages", icon: "pi pi-fw pi-spinner", to: "/messages" },
-          { label: "Charts", icon: "pi pi-fw pi-chart-bar", to: "/charts" },
-          { label: "Misc", icon: "pi pi-fw pi-upload", to: "/misc" },
-        ],
-      },
-      {
-        label: "Template Pages",
-        icon: "pi pi-fw pi-file",
-        items: [{ label: "Empty Page", icon: "pi pi-fw pi-circle-off", to: "/empty" }],
-      },
-      {
-        label: "Menu Hierarchy",
-        icon: "pi pi-fw pi-search",
-        items: [
-          {
-            label: "Submenu 1",
-            icon: "pi pi-fw pi-bookmark",
-            items: [
-              {
-                label: "Submenu 1.1",
-                icon: "pi pi-fw pi-bookmark",
-                items: [
-                  { label: "Submenu 1.1.1", icon: "pi pi-fw pi-bookmark" },
-                  { label: "Submenu 1.1.2", icon: "pi pi-fw pi-bookmark" },
-                  { label: "Submenu 1.1.3", icon: "pi pi-fw pi-bookmark" },
-                ],
-              },
-              {
-                label: "Submenu 1.2",
-                icon: "pi pi-fw pi-bookmark",
-                items: [
-                  { label: "Submenu 1.2.1", icon: "pi pi-fw pi-bookmark" },
-                  { label: "Submenu 1.2.2", icon: "pi pi-fw pi-bookmark" },
-                ],
-              },
-            ],
-          },
-          {
-            label: "Submenu 2",
-            icon: "pi pi-fw pi-bookmark",
-            items: [
-              {
-                label: "Submenu 2.1",
-                icon: "pi pi-fw pi-bookmark",
-                items: [
-                  { label: "Submenu 2.1.1", icon: "pi pi-fw pi-bookmark" },
-                  { label: "Submenu 2.1.2", icon: "pi pi-fw pi-bookmark" },
-                  { label: "Submenu 2.1.3", icon: "pi pi-fw pi-bookmark" },
-                ],
-              },
-              {
-                label: "Submenu 2.2",
-                icon: "pi pi-fw pi-bookmark",
-                items: [
-                  { label: "Submenu 2.2.1", icon: "pi pi-fw pi-bookmark" },
-                  { label: "Submenu 2.2.2", icon: "pi pi-fw pi-bookmark" },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        label: "Documentation",
-        icon: "pi pi-fw pi-question",
-        command: () => {
-          window.location = "#/documentation";
-        },
-      },
-      {
-        label: "View Source",
-        icon: "pi pi-fw pi-search",
-        command: () => {
-          window.location = "https://github.com/primefaces/sigma";
-        },
-      },
-    ];
   }
 
   addClass(element, className) {
